ca1b0e576e6e8f043ec26f221760a5b27a824758
Atdhe Boshnjaku <atdhe.boshnjaku@gmail.com>
2023-02-23 15:09:52 +0100

    Update

--- AirplaneTicket/ViewController.swift
+++ AirplaneTicket/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
         departureAndArrivalView.layer.shadowColor = UIColor(red: 111.0/255, green: 54.0/255, blue: 218.0/255, alpha: 1.0).cgColor
         departureAndArrivalView.layer.shadowOpacity = 0.5
         departureAndArrivalView.layer.shadowOffset = CGSize(width: 0, height: 3)
-        departureAndArrivalView.layer.shadowRadius = 4
+        departureAndArrivalView.layer.shadowRadius = 5
         departureAndArrivalView.layer.shouldRasterize = true
         
         let radians = 2 * Double.pi / 180
